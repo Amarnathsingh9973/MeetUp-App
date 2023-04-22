3a8e6b124ff16f2b99158f2509641cbdb726e413
Amar Nath Singh <Amarnathsingh7277@gmail.com>
2023-04-22 11:55:36 +0530

    let allmeetup as default and change favicon and added some code for hosting the website

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,10 @@ function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<AllMeetUpsPage />} exact></Route>
+        <Route path="/" element={<AllMeetUpsPage />}></Route>
         <Route path="/new_meetup" element={<NewMeetUpPage />}></Route>
         <Route path="/favorite" element={<FavoritesPage />}></Route>
+        <Route path="*" element={<AllMeetUpsPage />} />
       </Routes>
     </Layout>
   );
